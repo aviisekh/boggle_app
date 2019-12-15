0f42ab2ccd9afb84c69931d4cdc5e023b529ffa6
Abhishek Bhatta <aviisekh@gmail.com>
2019-12-15 13:08:03 +0545

    added the controller

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,8 @@
+class GameController < ApplicationController
+  def new
+    #board = Board.new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   #
   #
   root to: 'dashboard#index'
+  resources :game, only: [:new, :show]
 end
