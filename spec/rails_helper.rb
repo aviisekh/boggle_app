6c5759408e04d70ae73b6817c508d0cbe4bba4ad
Abhishek Bhatta <aviisekh@gmail.com>
2019-12-15 12:29:26 +0545

    configured and added the tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda-matchers'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,6 +32,17 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# Config for Shoulda matcher
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+# Choose a test framework:
+    with.test_framework :rspec
+# choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
