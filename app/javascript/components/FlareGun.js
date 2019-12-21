49b755b44c19924a18af8969dbfb90bc35e95e01
Abhishek Bhatta <aviisekh@gmail.com>
2019-12-21 14:52:55 +0545

    renamed the files and added method to start the game

--- app/javascript/components/FlareGun.js
+++ app/javascript/components/FlareGun.js
@@ -2,7 +2,6 @@ import React from "react"
 
 
 const FlareGun = ({ startGame }) => {
-
   return(
     <div className="flaregun">
       <button type="button" onClick={startGame} className="btn btn-lg start-button">Start New Game</button>
